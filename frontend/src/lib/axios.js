1220bf03830d9a5193e371d98a52c845b6abec61
Ashish Kumar <ak7979939461@gmail.com>
2026-01-02 23:17:45 +0530

    Fixed base_URL typo

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 //for handling the api calls
 export const axiosInstance = axios.create({
-    baseURL: import.meta.end.MODE === "development"?"http://localhost:5001/api":"/api", 
+    baseURL: import.meta.env.MODE === "development"?"http://localhost:5001/api":"/api", 
     withCredentials:true, // used to send the cookies with the also
 })
